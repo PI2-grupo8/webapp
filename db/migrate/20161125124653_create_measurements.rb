44b9d4a2df7dda5183ffc5d183e5ad5aed54d8f2
Leonardo Cambraia <cambraia@protonmail.com>
2016-11-25 10:48:50 -0200

    Generating measurement model files

---
+++ db/migrate/20161125124653_create_measurements.rb
@@ -0,0 +1,17 @@
+class CreateMeasurements < ActiveRecord::Migration[5.0]
+  def change
+    create_table :measurements do |t|
+      t.integer :id
+      t.time :horario
+      t.date :dia
+      t.float :latitude
+      t.float :longitude
+      t.float :umidade_solo
+      t.float :umidade_solo_calibrada
+      t.float :umidade_ar
+      t.float :temperatura_ar
+
+      t.timestamps
+    end
+  end
+end
