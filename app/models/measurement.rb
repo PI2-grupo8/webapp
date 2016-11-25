d830eb212388bdc56e8123855678e9c03f67e6a3
Leonardo Cambraia <cambraia@protonmail.com>
2016-11-25 11:00:51 -0200

    Creating import method in Measurement model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,2 +1,9 @@
 class Measurement < ApplicationRecord
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(public/measurement, headers: true) do |row|
+      Measurement.create! row.to_hash
+    end
+  end
 end
