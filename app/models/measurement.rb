861a68d039fb3c004650f7c00cbc0337e310db6b
Leonardo Cambraia <cambraia@protonmail.com>
2016-11-26 12:01:07 -0200

    Adding button to read the csv

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,19 +1,12 @@
 class Measurement < ApplicationRecord
-  # require 'csv'
-  #
-  # def self.import(file)
-  #   CSV.foreach(public/measurements/mock.csv, headers: true) do |row|
-  #     Measurement.create! row.to_hash
-  #   end
-  # end
-
   require 'smarter_csv'
 
+  def self.import(file)
     options = {}
     SmarterCSV.process('public/measurements/mock.csv', options) do |chunk|
       chunk.each do |data_hash|
         Measurement.create!( data_hash )
       end
     end
-
+  end
 end
