26a65f098080ede96cded08fefdac4c010d89f15
Leonardo Cambraia <cambraia@protonmail.com>
2016-12-02 17:34:32 -0200

    Setting heatmap data

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -9,6 +9,21 @@ class MeasurementsController < ApplicationController
   end
 
   def humidity_heatmap
+    @measurements = Measurement.all
+
+    latitude_array = []
+    longitude_array = []
+    humidity_array = []
+
+    @measurements.each_with_index do |measurement, humidity_heatmap|
+      latitude_array.append(measurement.latitude)
+      longitude_array.append(measurement.longitude)
+      humidity_array.append(measurement.absolute_humidity)
+    end
+
+    @js_lat = latitude_array
+    @js_long = longitude_array
+    @js_humidity = humidity_array
 
   end
 end
