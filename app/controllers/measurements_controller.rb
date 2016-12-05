34b7f17bbe9d021433fef6ccdf1d746da9967d2c
Leonardo Cambraia <cambraia@protonmail.com>
2016-12-05 00:09:17 -0200

    Creating several types of heatmaps

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -24,6 +24,41 @@ class MeasurementsController < ApplicationController
     @js_lat = latitude_array
     @js_long = longitude_array
     @js_humidity = humidity_array
+  end
 
+  def air_humidity_heatmap
+    @measurements = Measurement.all
+
+    latitude_array = []
+    longitude_array = []
+    air_humidity_array = []
+
+    @measurements.each_with_index do |measurement, air_humidity_heatmap|
+      latitude_array.append(measurement.latitude)
+      longitude_array.append(measurement.longitude)
+      air_humidity_array.append(measurement.air_humidity)
+    end
+
+    @js_lat = latitude_array
+    @js_long = longitude_array
+    @js_air_humidity = air_humidity_array
+  end
+
+  def air_temperature_heatmap
+    @measurements = Measurement.all
+
+    latitude_array = []
+    longitude_array = []
+    air_temperature_array = []
+
+    @measurements.each_with_index do |measurement, air_temperature_heatmap|
+      latitude_array.append(measurement.latitude)
+      longitude_array.append(measurement.longitude)
+      air_temperature_array.append(measurement.air_temperature)
+    end
+
+    @js_lat = latitude_array
+    @js_long = longitude_array
+    @js_air_temperature = air_temperature_array
   end
 end
